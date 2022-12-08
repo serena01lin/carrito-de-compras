11d53cb6a84353e0cbb59421b5fff22c6f69aa9f
Serena Lin <116319416+serena01lin@users.noreply.github.com>
2022-12-08 11:06:27 -0300

    correccion

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
-let comidas = []
-let comida = prompt("🍕Hola! Que comida deseas pedir?🥙");
+const comidas = [];
+const comida = prompt("🍕Hola! Que comida deseas pedir?🥙");
 comidas.push(comida);
 
 while (confirm("Desea pedir otra comida?🛒")){
-    prompt("¿Qué comida desea?");
+    const comida = prompt("¿Qué comida desea?");
     comidas.push(comida);
-}
+};
  console.log("Usted compró: ");
- comidas.forEach(comida => console.log(comida));
+ for (let comida of comidas) {
+    console.log(comida);
+ };
